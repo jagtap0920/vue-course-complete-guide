225fd9b04f97d80f0c8eb47f01549cf7dd2a1a7f
Tushar Jagtap <jagtap0920@gmail.com>
2021-07-01 11:01:03 +0530

    [TASK] Added coaches list card with details & contact form

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,15 +1,30 @@
 <template>
-  <h1>Coaches List</h1>
+  <div class="controls">
+    <button>Refresh</button>
+    <router-link to="/register">Register as Coach</router-link>
+  </div>
   <ul v-if="hasCoaches">
-    <li v-for="coach in filteredCoaches" :key="coach.id">
-      {{ coach.firstName }}
-    </li>
+    <coach-item
+      v-for="coach in filteredCoaches"
+      :key="coach.id"
+      :id="coach.id"
+      :firstName="coach.firstName"
+      :lastName="coach.lastName"
+      :areas="coach.areas"
+      :description="coach.description"
+      :rate="coach.hourlyRate"
+    >
+    </coach-item>
   </ul>
   <h3 v-else>No coaches found</h3>
 </template>
 
 <script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
 export default {
+  components: {
+    CoachItem,
+  },
   computed: {
     filteredCoaches() {
       return this.$store.getters['coaches/coaches'];
@@ -21,4 +36,15 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
