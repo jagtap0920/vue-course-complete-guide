d741ebed0bfb93fddb9a8546764d9c6da617a4e0
Tushar Jagtap <jagtap0920@gmail.com>
2021-07-01 21:23:35 +0530

    [TASK] Added base button, card & badge components

--- src/main.js
+++ src/main.js
@@ -4,9 +4,17 @@ import App from './App.vue';
 import router from './routes/router.js';
 import store from './store/index.js';
 
+import BaseCard from './ui/BaseCard.vue';
+import BaseButton from './ui/BaseButton.vue';
+import BaseBadge from './ui/BaseBadge.vue';
+
 const app = createApp(App);
 
 app.use(router);
 app.use(store);
+
+app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
+app.component('base-badge', BaseBadge);
 
 app.mount('#app');
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,29 +1,33 @@
 <template>
-  <div class="controls">
-    <button>Refresh</button>
-    <router-link to="/register">Register as Coach</router-link>
-  </div>
-  <ul v-if="hasCoaches">
-    <coach-item
-      v-for="coach in filteredCoaches"
-      :key="coach.id"
-      :id="coach.id"
-      :firstName="coach.firstName"
-      :lastName="coach.lastName"
-      :areas="coach.areas"
-      :description="coach.description"
-      :rate="coach.hourlyRate"
-    >
-    </coach-item>
-  </ul>
-  <h3 v-else>No coaches found</h3>
+  <base-card>
+    <div class="controls">
+      <base-button mode="outline">Refresh</base-button>
+      <base-button link to="/register">Register as Coach</base-button>
+    </div>
+    <ul v-if="hasCoaches">
+      <coach-item
+        v-for="coach in filteredCoaches"
+        :key="coach.id"
+        :id="coach.id"
+        :firstName="coach.firstName"
+        :lastName="coach.lastName"
+        :areas="coach.areas"
+        :description="coach.description"
+        :rate="coach.hourlyRate"
+      >
+      </coach-item>
+    </ul>
+    <h3 v-else>No coaches found</h3>
+  </base-card>
 </template>
 
 <script>
 import CoachItem from '../../components/coaches/CoachItem.vue';
+import BaseButton from '../../ui/BaseButton.vue';
 export default {
   components: {
     CoachItem,
+    BaseButton,
   },
   computed: {
     filteredCoaches() {
