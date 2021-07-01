3f1d44a3acb1abda7fa7d5304003b647bb214682
Tushar Jagtap <jagtap0920@gmail.com>
2021-07-01 09:05:46 +0530

    [TASK] Added coaches dummy data

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,9 +1,24 @@
 <template>
   <h1>Coaches List</h1>
+  <ul v-if="hasCoaches">
+    <li v-for="coach in filteredCoaches" :key="coach.id">
+      {{ coach.firstName }}
+    </li>
+  </ul>
+  <h3 v-else>No coaches found</h3>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    },
+  },
+};
 </script>
 
 <style></style>
--- src/store/coaches/index.js
+++ src/store/coaches/index.js
@@ -5,7 +5,27 @@ import getters from './getters.js';
 export default {
   namespaced: true,
   state() {
-    return {};
+    return {
+      coaches: [
+        {
+          id: 'c1',
+          firstName: 'Tushar',
+          lastName: 'Jagtap',
+          areas: ['frontend', 'backend', 'career'],
+          description: 'He is an great full stack developer',
+          hourlyRate: 50,
+        },
+        {
+          id: 'c2',
+          firstName: 'Dinesh',
+          lastName: 'Rabara',
+          areas: ['frontend', 'backend'],
+          description:
+            'He is an great full stack developer and good in nature!!',
+          hourlyRate: 100,
+        },
+      ],
+    };
   },
   mutations,
   actions,
